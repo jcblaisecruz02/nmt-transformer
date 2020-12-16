f83ee549b54afbfb4303ba2681bb31b544a5c032
jcblaisecruz02 <jan_christian_cruz@dlsu.edu.ph>
2020-12-16 03:32:06 +0000

    Added checkpointing.

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ import torch.nn as nn
 import torch.optim as optim
 
 import numpy as np
-import os
-from os import path
-from tqdm import tqdm
 import argparse
 
 from utils.data import TextDataset, collate_fn
 from utils.model import Encoder, Decoder, Seq2Seq, count_parameters
+from utils.train import train, evaluate, save_checkpoint
 
 try:
     from apex import amp 
@@ -17,82 +15,51 @@ try:
 except ModuleNotFoundError:
     APEX_AVAILABLE = False
 
-def train(model, criterion, optimizer, train_loader, device=None, clip=0.0, scheduler=None, fp16=False):
-    train_loss = 0
-    model.train()
-    for x, y in tqdm(train_loader):
-        x, y = x.to(device), y.to(device)
-
-        out, _ = model(x, y[:, :-1])
-        loss = criterion(out.contiguous().flatten(0, 1), 
-                        y[:, 1:].contiguous().flatten(0))
-                
-        optimizer.zero_grad()
-
-        if fp16 and APEX_AVAILABLE:
-            with amp.scale_loss(loss, optimizer) as scaled_loss:
-                scaled_loss.backward() 
-                nn.utils.clip_grad_norm_(amp.master_params(optimizer), clip)
-        else: 
-            loss.backward()
-            nn.utils.clip_grad_norm_(model.parameters(), clip)
-
-        optimizer.step()
-        if scheduler is not None: scheduler.step()
-
-        train_loss += loss.item()
-    train_loss /= len(train_loader) 
-    return train_loss
-
-def evaluate(model, criterion, valid_loader, device=None):
-    valid_loss = 0
-    model.eval()
-    for x, y in tqdm(valid_loader):
-        with torch.no_grad():
-            x, y = x.to(device), y.to(device)
-
-            out, _ = model(x, y[:, :-1])
-            loss = criterion(out.contiguous().flatten(0, 1), 
-                            y[:, 1:].contiguous().flatten(0))
-
-            valid_loss += loss.item()
-    valid_loss /= len(valid_loader) 
-    return valid_loss
-
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--train_dir', type=str)
-    parser.add_argument('--valid_dir', type=str)
-    parser.add_argument('--test_dir', type=str)
-    parser.add_argument('--src_vocab', type=str)
-    parser.add_argument('--trg_vocab', type=str)
-    parser.add_argument('--src_msl', type=int, default=100)
-    parser.add_argument('--trg_msl', type=int, default=100)
-    parser.add_argument('--batch_size', type=int, default=8)
-    parser.add_argument('--num_workers', type=int, default=1)
+    parser.add_argument('--train_dir', type=str, help='Directory of training samples')
+    parser.add_argument('--valid_dir', type=str, help='Directory of validation samples')
+    parser.add_argument('--test_dir', type=str, help='Directory of test samples')
+    parser.add_argument('--src_vocab', type=str, help='SentencePiece vocabulary file for source sentence')
+    parser.add_argument('--trg_vocab', type=str, help='SentencePiece vocabulary file for target sentence')
+    parser.add_argument('--src_msl', type=int, default=100, help='Maximum sequence length for source sentence')
+    parser.add_argument('--trg_msl', type=int, default=100, help='Maximum sequence length for target sentence')
+    parser.add_argument('--batch_size', type=int, default=8, help='Batch size')
+    parser.add_argument('--num_workers', type=int, default=1, help='Number of parallel workers for dataloading')
+    parser.add_argument('--save_dir', type=str, help='Directory to save checkpoints and load checkpoints from')
 
-    parser.add_argument('--save_dir', type=str)
+    parser.add_argument('--epochs', type=int, default=10, help='Number of epochs to train')
+    parser.add_argument('--hidden_dim', type=int, default=256, help='Hidden dimensions of the transformer layers')
+    parser.add_argument('--n_layers', type=int, default=3, help='Number of transformer blocks in the encoder and decoder')
+    parser.add_argument('--n_heads', type=int, default=8, help='Number of attention heads')
+    parser.add_argument('--pf_dim', type=int, default=512, help='Positionwise feedforward projection dimension')
+    parser.add_argument('--dropout', type=float, default=0.1, help='Dropout probability')
+    parser.add_argument('--clip', type=float, default=1.0, help='Gradient clipping')
 
-    parser.add_argument('--epochs', type=int, default=10)
-    parser.add_argument('--hidden_dim', type=int, default=256)
-    parser.add_argument('--n_layers', type=int, default=3)
-    parser.add_argument('--n_heads', type=int, default=8)
-    parser.add_argument('--pf_dim', type=int, default=512)
-    parser.add_argument('--dropout', type=float, default=0.1)
-    parser.add_argument('--learning_rate', type=float, default=3e-4)
-    parser.add_argument('--clip', type=float, default=0.1)
+    parser.add_argument('--optimizer', type=str, default='adam', choices=['adam', 'adamw', 'lamb'], help='Optimizer to use')
+    parser.add_argument('--learning_rate', type=float, default=3e-4, help='Learning rate')
+    parser.add_argument('--weight_decay', type=float, default=0.0, help='Weight decay for non LayerNorm and Bias layers')
+    parser.add_argument('--adam_epsilon', type=float, default=1e-8, help='Epsilon value for Adam')
+    parser.add_argument('--adam_b1', type=float, default=0.9, help='Beta1 for LAMB')
+    parser.add_argument('--adam_b2', type=float, default=0.99, help='Beta2 for LAMB')
+    parser.add_argument('--scheduler', type=str, default=None, choices=['cosine', 'linear', None], help='Scheduler to use')
+    parser.add_argument('--warmup_pct', type=float, default=0.1, help='Percentage of steps to warmup for linear scheduler')
     
-    parser.add_argument('--do_train', action='store_true')
-    parser.add_argument('--do_test', action='store_true')
-    parser.add_argument('--no_cuda', action='store_true')
-    parser.add_argument('--fp16', action='store_true')
-    parser.add_argument('--opt_level', type=str, default='O1')
+    parser.add_argument('--do_train', action='store_true', help='Train a model')
+    parser.add_argument('--do_test', action='store_true', help='Evaluate a model')
+    parser.add_argument('--resume_training', action='store_true', help='Toggle to resume from checkpoint in --save_dir')
+    parser.add_argument('--no_cuda', action='store_true', help='Do not use GPU')
+    parser.add_argument('--fp16', action='store_true', help='Use FP16 training via APEX')
+    parser.add_argument('--opt_level', type=str, default='O1', choices=['O1', 'O2'], help='Optimization level for FP16 training')
+    parser.add_argument('--save_every', type=int, default=1, help='Save checkpoint every --save_every epoch')
+    parser.add_argument('--pad_token', type=str, default='<pad>', help='Override default padding token')
 
-    parser.add_argument('--seed', type=int, default=1111)
+    parser.add_argument('--seed', type=int, default=1111, help='Random seed')
     
     args = parser.parse_args()
     print(args)
 
+    # Set seeds
     torch.manual_seed(args.seed)
     device = torch.device('cuda' if torch.cuda.is_available() and not args.no_cuda else 'cpu')
     
@@ -116,7 +83,7 @@ def main():
 
         print("Training batches: {}\nValidation batches: {}".format(len(train_loader), len(valid_loader)))
 
-        # Produce training setup
+        # Produce model and criterion
         encoder = Encoder(vocab_sz=train_dataset.src_vocab_sz, 
                           hidden_dim=args.hidden_dim, 
                           n_layers=args.n_layers, 
@@ -133,36 +100,92 @@ def main():
                           dropout=args.dropout, 
                           msl=args.trg_msl, 
                           fp16=args.fp16)
-        model = Seq2Seq(encoder, decoder, train_dataset.src_word2idx['<pad>'], train_dataset.trg_word2idx['<pad>']).to(device)
+        model = Seq2Seq(encoder, decoder, train_dataset.src_word2idx[args.pad_token], train_dataset.trg_word2idx[args.pad_token]).to(device)
+        criterion = nn.CrossEntropyLoss(ignore_index=train_dataset.src_word2idx[args.pad_token])
 
-        criterion = nn.CrossEntropyLoss(ignore_index=train_dataset.src_word2idx['<pad>'])
-        optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
+        # Produce Optimizer
+        no_decay = ["bias", "LayerNorm.weight"]
+        optimizer_grouped_parameters = [{"params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)], 
+                                         "weight_decay": args.weight_decay}, 
+                                        {"params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 
+                                         "weight_decay": 0.0}]
 
+        if args.optimizer == 'adamw':
+            try:
+                from transformers import AdamW
+                optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
+            except ModuleNotFoundError:
+                print("Transformers module not found for AdamW. Using generic Adam instead.")
+                optimizer = optim.Adam(optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
+        elif args.optimizer == 'lamb':
+            try:
+                from pytorch_lamb import Lamb
+                optimizer = Lamb(model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay, betas=(args.adam_b1, args.adam_b2))
+            except ModuleNotFoundError:
+                print("LAMB implementation not found. Using generic Adam instead.")
+                optimizer = optim.Adam(optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
+        elif args.optimizer == 'adam':
+            optimizer = optim.Adam(optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
+
+        # Configure FP16
         if args.fp16 and APEX_AVAILABLE:
             model, optimizer = amp.initialize(model, optimizer, opt_level=args.opt_level)
 
-        scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, len(train_loader) * args.epochs)
+        # Produce the scheduler
+        if args.scheduler == 'cosine':
+            scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, len(train_loader) * args.epochs)
+        elif args.scheduler == 'linear':
+            try:
+                from transformers import get_linear_schedule_with_warmup
+                steps = args.epochs * len(train_loader)
+                scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=int(steps * args.warmup_pct), num_training_steps=steps)
+            except ModuleNotFoundError:
+                print('Transformers module not found for Linear Schedule. Not using a scheduler instead.')
+                scheduler = None
+        else:
+            scheduler = None
 
-        print("Model has {:,} trainable parameters.".format(count_parameters(model)))
+        print("\nModel has {:,} trainable parameters.\n".format(count_parameters(model)))
+
+        # Configure states if resuming from checkpoint
+        if args.resume_training:
+            print("Loading from checkpoint...", end='')
+            with open(args.save_dir + '/model.bin', 'rb') as f:
+                model.load_state_dict(torch.load(f))
+                print('Model loaded...', end='')
+            with open(args.save_dir + '/training.bin', 'rb') as f:
+                training_state = torch.load(f)
+                optimizer.load_state_dict(training_state['opt_state'])
+                e = training_state['e'] + 1 # Start on the next epoch
+                print('Optimizer loaded...', end='')
+
+                if training_state['scheduler'] is not None:
+                    scheduler.load_state_dict(training_state['scheduler'])
+                    print('Scheduler loaded...', end='')
+                else:
+                    print('No scheduler found...')
+            print("Done!\nLoaded checkpoint from epoch {}!\n".format(training_state['e']))
+        
+        # Else, begin from epoch 1
+        else:
+            print("Beginning training from epoch 1.\n")
+            e = 1
 
         # Train Model
-        print("Beginning Training.")
-        for e in range(1, args.epochs + 1):
+        while e < args.epochs + 1:
+            # Train one epoch
             train_loss = train(model, criterion, optimizer, train_loader, device=device, clip=args.clip, scheduler=scheduler, fp16=args.fp16)
             valid_loss = evaluate(model, criterion, valid_loader, device=device)
             print("Epoch {:3} | Train Loss {:.4f} | Train Ppl {:.4f} | Valid Loss {:.4f} | Valid Ppl {:.4f}".format(e, train_loss, np.exp(train_loss), valid_loss, np.exp(valid_loss)))
 
-        # Save the model and states
-        print("Saving checkpoint.")
-        if path.exists(args.save_dir):
-            print("Save directory exists. Deleting.")
-            os.system('rm -r ' + args.save_dir)
-        model = model.cpu()
-        os.mkdir(args.save_dir)
-        with open(args.save_dir + '/model.bin', 'wb') as f:
-            torch.save(model.state_dict(), f)
-        with open(args.save_dir + '/settings.bin', 'wb') as f:
-            torch.save([args.hidden_dim, args.n_layers, args.n_heads, args.pf_dim, args.dropout, args.src_msl, args.trg_msl], f)
+            # Save the checkpoint
+            if e % args.save_every == 0 or e == args.epochs:
+                print('Saving checkpoint for epoch {}...'.format(e), end='')
+                save_checkpoint(model, args, optimizer=optimizer, e=e, scheduler=scheduler, save_state=True)
+                print('Done!')
+            
+            # Update epoch number
+            e += 1
 
         print("Training done!\n")
 
@@ -182,7 +205,7 @@ def main():
         print("Loading model and saved settings.")
         with open(args.save_dir + '/settings.bin', 'rb') as f:
             hd, nl, nh, pf, dp, smsl, tmsl = torch.load(f)
-        print(test_dataset.src_vocab_sz)
+
         encoder = Encoder(vocab_sz=test_dataset.src_vocab_sz, 
                           hidden_dim=hd, 
                           n_layers=nl, 
@@ -199,19 +222,17 @@ def main():
                           dropout=dp, 
                           msl=tmsl, 
                           fp16=args.fp16)
-        model = Seq2Seq(encoder, decoder, test_dataset.src_word2idx['<pad>'], test_dataset.trg_word2idx['<pad>'])
+        model = Seq2Seq(encoder, decoder, test_dataset.src_word2idx[args.pad_token], test_dataset.trg_word2idx[args.pad_token])
+        criterion = nn.CrossEntropyLoss(ignore_index=test_dataset.src_word2idx[args.pad_token])
 
+        # Load the checkpoint
         with open(args.save_dir + '/model.bin', 'rb') as f:
             model.load_state_dict(torch.load(f))
         model = model.to(device)
-
-        criterion = nn.CrossEntropyLoss(ignore_index=test_dataset.src_word2idx['<pad>'])
         
         print("Begin testing.")
         test_loss = evaluate(model, criterion, test_loader, device=device)
         print("Test Loss {:.4f} | Test Ppl {:.4f}".format(test_loss, np.exp(test_loss)))
-        
-        
 
 if __name__ == '__main__':
     main()
---
+++ utils/train.py
@@ -0,0 +1,95 @@
+import torch 
+import torch.nn as nn
+import numpy as np 
+import os
+from os import path
+from tqdm import tqdm
+import warnings
+
+try:
+    from apex import amp 
+    APEX_AVAILABLE = True 
+except ModuleNotFoundError:
+    APEX_AVAILABLE = False
+
+def train(model, criterion, optimizer, train_loader, device=None, clip=0.0, scheduler=None, fp16=False):
+    train_loss = AverageMeter()
+    model.train()
+
+    with tqdm(train_loader) as pbar:
+        for x, y in pbar:
+            x, y = x.to(device), y.to(device)
+
+            out, _ = model(x, y[:, :-1])
+            loss = criterion(out.contiguous().flatten(0, 1), 
+                            y[:, 1:].contiguous().flatten(0))
+                    
+            optimizer.zero_grad()
+
+            # Handle backprop, then step opt and scheduler
+            if fp16 and APEX_AVAILABLE:
+                with amp.scale_loss(loss, optimizer) as scaled_loss:
+                    scaled_loss.backward() 
+                    nn.utils.clip_grad_norm_(amp.master_params(optimizer), clip)
+            else: 
+                loss.backward()
+                nn.utils.clip_grad_norm_(model.parameters(), clip)
+            optimizer.step()
+            if scheduler is not None: scheduler.step()
+
+            # Set progress bar information
+            pbar.set_postfix({'lr': optimizer.param_groups[0]['lr'], 
+                              'loss': train_loss.avg})
+
+            train_loss.update(loss.item(), x.shape[0]) 
+    return train_loss.avg
+
+def evaluate(model, criterion, valid_loader, device=None):
+    valid_loss = AverageMeter()
+    model.eval()
+
+    for x, y in tqdm(valid_loader):
+        with torch.no_grad():
+            x, y = x.to(device), y.to(device)
+
+            out, _ = model(x, y[:, :-1])
+            loss = criterion(out.contiguous().flatten(0, 1), 
+                            y[:, 1:].contiguous().flatten(0))
+
+            valid_loss.update(loss.item(), x.shape[0])
+    return valid_loss.avg
+
+def save_checkpoint(model, args, optimizer=None, e=None, scheduler=None, save_state=False):
+    if path.exists(args.save_dir): os.system('rm -r ' + args.save_dir)
+    os.mkdir(args.save_dir)
+
+    with open(args.save_dir + '/model.bin', 'wb') as f:
+        torch.save(model.state_dict(), f)
+    with open(args.save_dir + '/settings.bin', 'wb') as f:
+        torch.save([args.hidden_dim, args.n_layers, args.n_heads, args.pf_dim, args.dropout, args.src_msl, args.trg_msl], f)
+    
+    # Save the optimizer state for training
+    if save_state and optimizer is not None:
+        with warnings.catch_warnings(): # Ignore initial "please save opt and scheduler" warning
+            warnings.simplefilter('ignore')
+            if scheduler is not None: opt = {'opt_state': optimizer.state_dict(), 'scheduler': scheduler.state_dict(), 'e': e}
+            else: opt = {'opt_state': optimizer.state_dict(), 'scheduler': None, 'e': e}
+            with open(args.save_dir + '/training.bin', 'wb') as f:
+                torch.save(opt, f)
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self, fmt=':f'):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
