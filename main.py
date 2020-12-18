Fixed bugs with BLEU computation and label smoothing

main.py
@@ -121,7 +121,7 @@ def main():
         if args.criterion == 'cross_entropy':
             criterion = nn.CrossEntropyLoss(ignore_index=train_dataset.src_word2idx[args.pad_token])
         elif args.criterion == 'label_smoothing':
-            criterion = LabelSmoothingLoss(train_dataset.trg_vocab_sz, smoothing=args.smoothing)
+            criterion = LabelSmoothingLoss(epsilon=args.smoothing)
 
         # Produce Optimizer
         no_decay = ["bias", "LayerNorm.weight"]
@@ -280,7 +280,7 @@ def main():
         if cri == 'cross_entropy':
             criterion = nn.CrossEntropyLoss(ignore_index=test_dataset.trg_word2idx[args.pad_token])
         elif cri == 'label_smoothing':
-            criterion = LabelSmoothingLoss(test_dataset.trg_vocab_sz, smoothing=args.smoothing)
+            criterion = LabelSmoothingLoss(epsilon=args.smoothing)
 
         # Load the checkpoint
         if usw:

utils/optim.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 from torch.optim.lr_scheduler import _LRScheduler
+from torch.autograd import Variable
+import torch.nn.functional as F
 
 class NoamLR(_LRScheduler):
     def __init__(self, optimizer, warmup_steps):
@@ -12,18 +14,21 @@ class NoamLR(_LRScheduler):
         scale = self.warmup_steps ** 0.5 * min(last_epoch ** (-0.5), last_epoch * self.warmup_steps ** (-1.5))
         return [base_lr * scale for base_lr in self.base_lrs]
 
-class LabelSmoothingLoss(nn.Module):
-    def __init__(self, classes, smoothing=0.0, dim=-1):
-        super(LabelSmoothingLoss, self).__init__()
-        self.confidence = 1.0 - smoothing
-        self.smoothing = smoothing
-        self.cls = classes
-        self.dim = dim
+def linear_combination(x, y, epsilon): 
+    return epsilon * x + (1 - epsilon) * y
 
-    def forward(self, pred, target):
-        pred = pred.log_softmax(dim=self.dim)
-        with torch.no_grad():
-            true_dist = torch.zeros_like(pred)
-            true_dist.fill_(self.smoothing / (self.cls - 1))
-            true_dist.scatter_(1, target.data.unsqueeze(1), self.confidence)
-        return torch.mean(torch.sum(-true_dist * pred, dim=self.dim))
+def reduce_loss(loss, reduction='mean'):
+    return loss.mean() if reduction == 'mean' else loss.sum() if reduction == 'sum' else loss
+
+class LabelSmoothingLoss(nn.Module):
+    def __init__(self, epsilon=0.1, reduction='mean'):
+        super(LabelSmoothingLoss, self).__init__()
+        self.epsilon = epsilon
+        self.reduction = reduction
+    
+    def forward(self, preds, target):
+        n = preds.size()[-1]
+        log_preds = F.log_softmax(preds, dim=-1)
+        loss = reduce_loss(-log_preds.sum(dim=-1), self.reduction)
+        nll = F.nll_loss(log_preds, target, reduction=self.reduction)
+        return linear_combination(loss/n, nll, self.epsilon)